ee08029c191c26c7c4ae48ef2d5fa0c2eedcfb61
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2026-02-17 09:06:10 +0530

    Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/webapp/resources/sql/correct_letters.sql
+++ src/main/webapp/resources/sql/correct_letters.sql
@@ -1,14 +1,14 @@
   -- Preview first
   SELECT COUNT(*) AS records_to_update
-  FROM documenthistory dh
-  JOIN webuser wu ON dh.TOUSER_ID = wu.ID
+  FROM DOCUMENTHISTORY dh
+  JOIN WEBUSER wu ON dh.TOUSER_ID = wu.ID
   WHERE dh.TOUSER_ID IS NOT NULL
     AND dh.TOINSTITUTION_ID IS NULL
     AND dh.HISTORYTYPE = 'Letter_Copy_or_Forward';
 
   -- Then update
-  UPDATE documenthistory dh
-  JOIN webuser wu ON dh.TOUSER_ID = wu.ID
+  UPDATE DOCUMENTHISTORY dh
+  JOIN WEBUSER wu ON dh.TOUSER_ID = wu.ID
   SET dh.TOINSTITUTION_ID = wu.INSTITUTION_ID
   WHERE dh.TOUSER_ID IS NOT NULL
     AND dh.TOINSTITUTION_ID IS NULL
